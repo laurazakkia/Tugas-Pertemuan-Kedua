9368fbe3fd6d2cbbd616c2aaa465b1e30135c256
laurazakkia <laurazakkia12@gmail.com>
2024-09-07 10:48:29 +0700

    Add files via upload

---
+++ Motor.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuankedua;
+
+/**
+ *
+ * @author Laura
+ */
+public class Motor extends Transportasi implements Berhenti, Berjalan{
+    private int tahunProduksi;
+    private String tipe;
+    
+    @Override
+    public void start(){
+        System.out.println("Motor mulai dinyalakan");
+    }
+    
+    @Override
+    public void stop(){
+        System.out.println("Motor dimatikan");
+    }
+    
+    public Motor(){
+        System.out.println("Halo aku conss motor");
+        this.setTahunProduksi(2018);
+        this.setTipe("Sport");
+        super.setNama("Beat");
+        super.setKapasitas(2);
+    } 
+    
+    public void BahanBakar(){
+        System.out.println("Pertalite");
+    }
+    
+    public void MacamBensin(){
+        this.BahanBakar();
+        super.BahanBakar();
+    }
+    /**
+     * @return the tahunProduksi
+     */
+    public int getTahunProduksi() {
+        return tahunProduksi;
+    }
+
+    /**
+     * @param tahunProduksi the tahunProduksi to set
+     */
+    public void setTahunProduksi(int tahunProduksi) {
+        this.tahunProduksi = tahunProduksi;
+    }
+
+    /**
+     * @return the tipe
+     */
+    public String getTipe() {
+        return tipe;
+    }
+
+    /**
+     * @param tipe the tipe to set
+     */
+    public void setTipe(String tipe) {
+        this.tipe = tipe;
+    }
+}
---
+++ Utama.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuankedua;
+
+/**
+ *
+ * @author Laura
+ */
+public class Utama {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        Motor beat = new Motor();
+        System.out.println("Aku adalah motor " +beat.getNama() + " bertipe " +beat.getTipe() + " dengan kapasitas " +beat.getKapasitas() + " orang" + " dan aku diproduksi pada tahun " +beat.getTahunProduksi());
+        beat.start();
+        beat.stop();
+        beat.MacamBensin();
+        System.out.println("");
+        
+        Transportasi sepedaBagus = (Transportasi) beat;
+        System.out.println("Aku adalah sepeda bagus dengan kapasitas " +sepedaBagus.getKapasitas() + " orang");   
+    }
+}
